fb6b2ff883a23d29effb63b384f0c19f636c3381
tomato3713 <tomato3713@outlook.com>
2024-03-14 00:57:52 +0900

    fixed tab size and margins

--- src/app/components/Tab.tsx
+++ src/app/components/Tab.tsx
@@ -14,22 +14,24 @@ export function Tab({ tabs, defaultOpenKey }: Props) {
   };
 
   return (
-    <div className="flex flex-col">
-      <div className={`flex-initial w-50 flex-row divide-dashed`}>
-        {tabs.map((e) => {
-          return (
-            <button
-              key={`tab-item-${e.title}`}
-              className={`tab-header-item font-midium px-5 ${e.tabKey === tabKey && "bg-blue-300"}`}
-              onClick={handleClick}
-              value={e.tabKey}
-            >
-              {e.title}
-            </button>
-          );
-        })}
+    <div className="flex flex-col w-full md:w-10/12">
+      <div className="w-full justify-center">
+        <div className={`flex justify-center w-full`}>
+          {tabs.map((e) => {
+            return (
+              <button
+                key={`tab-item-${e.title}`}
+                className={`w-1/3 max-w-30 font-midium hover:font-bold text-xl ${e.tabKey === tabKey && "bg-blue-300"}`}
+                onClick={handleClick}
+                value={e.tabKey}
+              >
+                {e.title}
+              </button>
+            );
+          })}
+        </div>
       </div>
-      <div className="flex-col">
+      <div className="p-5">
         {tabs.find((e) => e.tabKey === tabKey)?.content}
       </div>
     </div>
