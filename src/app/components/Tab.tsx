b54dacbe0b26e85f75a0eb85fecce62a1e64b6b6
tomato3713 <tomato3713@outlook.com>
2024-03-14 13:56:28 +0900

    fixed tab header design

--- src/app/components/Tab.tsx
+++ src/app/components/Tab.tsx
@@ -16,12 +16,12 @@ export function Tab({ tabs, defaultOpenKey }: Props) {
   return (
     <div className="flex flex-col w-full md:w-10/12">
       <div className="w-full justify-center">
-        <div className={`flex justify-center w-full`}>
+        <div className={`flex justify-center w-full border-b border-blue-700`}>
           {tabs.map((e) => {
             return (
               <button
                 key={`tab-item-${e.title}`}
-                className={`w-1/3 max-w-30 font-midium hover:font-bold text-xl ${e.tabKey === tabKey && "bg-blue-300"}`}
+                className={`w-1/3 max-w-30 p-0 font-midium hover:font-bold me-2 border-b border-blue-200 rounded-t-lg text-l md:text-xl bg-blue-200 ${e.tabKey === tabKey && "bg-blue-700 text-gray-100"}`}
                 onClick={handleClick}
                 value={e.tabKey}
               >
