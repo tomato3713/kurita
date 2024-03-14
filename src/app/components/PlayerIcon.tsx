94d25985a8dbe83cdc7739bfb6afa1f0ba1339ca
tomato3713 <tomato3713@outlook.com>
2024-03-14 22:08:28 +0900

    design player icon

--- src/app/components/PlayerIcon.tsx
+++ src/app/components/PlayerIcon.tsx
@@ -5,9 +5,23 @@ type PlayerIconProps = Readonly<{
 }>;
 
 export const PlayerIcon: React.FC<PlayerIconProps> = ({ player }) => {
+  {
+  }
   return (
-    <div className={`mb-1 mt-1 mr-1 text-2xl font-semibold items-center`}>
-      Palyer {player.name}
-    </div>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill={player.color}
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke={player.color}
+      className={`aspect-square`}
+    >
+      <path
+        /* ref: https://heroicons.com/ */
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+      />
+    </svg>
   );
 };
